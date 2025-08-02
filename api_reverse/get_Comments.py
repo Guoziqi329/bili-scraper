2ad48dc9c4e4f31b1a7c2b40992862023d514cb0
guoziqi329 <guoziqi329@gmail.com>
2025-08-02 14:03:03 +0800

    add get_oid function

--- api_reverse/get_Comments.py
+++ api_reverse/get_Comments.py
@@ -1,21 +1,45 @@
 import requests
 import json
 import time
+import re
 from get_w_rid_And_wts import get_wbiImgKey_and_wbiSubKey, get_w_rid_And_wts, encodeURIComponent
 
 session = requests.Session()
 
 
+def get_oid(video_id: str, cookie) -> str:
+    """
+    get video oid by video id
+    :param video_id: the id in the url, such as BV1Mg8RzFExV
+    :return: video's oid
+    """
+    url = f"https://www.bilibili.com/video/{video_id}/"
+    headers = {
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36",
+        "cookie": cookie,
+    }
+    response = session.get(url, headers=headers)
+    return re.search(r'"aid":\d+',response.text)[0][6:]
+
+
 def get_offset(response) -> str:
+    """
+    get next offset
+    :param response: requests response
+    :return: next offset (str)
+    """
     return response.json()['data']['cursor']['pagination_reply']['next_offset']
 
 
 if __name__ == '__main__':
     with open('cookie.json', 'r') as f:
         cookie = json.load(f)['cookie']
-        
+
+    video_id = 'BV1Mg8RzFExV'
+    oid = get_oid(video_id, cookie)
+
     payload = {
-        "oid": "814225886",
+        "oid": oid,
         "type": "1",
         "mode": "3",
         "pagination_str": "{\"offset\":\"\"}",
@@ -38,12 +62,11 @@ if __name__ == '__main__':
 
     response = session.get("https://api.bilibili.com/x/v2/reply/wbi/main", headers=headers, params=payload)
     next_offset = get_offset(response)
-    # print(response.json())
     with open("test.json", "w", encoding='utf-8') as f:
         json.dump(response.json(), f)
 
     for item in response.json()['data']['replies']:
-        print(item['rpid'])
+        print(item['content']['message'])
         print('*'*50)
 
     print('-' * 100)
@@ -53,7 +76,7 @@ if __name__ == '__main__':
     for i in range(5):
 
         payload_2 = {
-            "oid": "814225886",
+            "oid": oid,
             "type": "1",
             "mode": "3",
             "pagination_str": "{\"offset\":\"" + next_offset + "\"}",
@@ -67,12 +90,11 @@ if __name__ == '__main__':
         payload_2["w_rid"] = w_rid
         payload_2["wts"] = wts
         response = session.get("https://api.bilibili.com/x/v2/reply/wbi/main", headers=headers, params=payload_2)
-        # print(response.json())
         with open(f"test{i}.json", "w", encoding='utf-8') as f:
             json.dump(response.json(), f)
 
         for item in response.json()['data']['replies']:
-            print(item['rpid'])
+            print(item['content']['message'])
             print('*'*50)
 
         print('-'*200)
