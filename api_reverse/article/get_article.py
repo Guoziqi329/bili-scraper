c0e05b62be68a325bb5b0dac34f9d69097ad2914
R8Ki0xx <wpengyu19@gmail.com>
2025-08-08 13:57:17 +0800

    add get_article.py

---
+++ api_reverse/article/get_article.py
@@ -0,0 +1,12 @@
+def get_article(cookie: str,article_id: str,storage_location: str = '.') -> None:
+    """
+    get_article
+    :param cookie: website's cookie information.
+    :param article_id: article's url such as 1097430290934005800 (https://www.bilibili.com/opus/1097430290934005800?spm_id_from=333.1387.0.0)
+    :param storage_location: local storage in word.
+    :return: None
+    """
+
+
+if __name__ == '__main__':
+    get_article()
