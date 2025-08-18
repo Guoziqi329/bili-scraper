dcb48fcbf518c57715333a67392a44f4a4c4464f
guoziqi329 <guoziqi329@gmail.com>
2025-08-18 18:58:12 +0800

    update get_article.py add new features for generating document.

--- api_reverse/article/get_article.py
+++ api_reverse/article/get_article.py
@@ -5,6 +5,9 @@ import re
 import os
 from datetime import datetime
 from lxml import etree
+from docx import Document
+from docx.shared import RGBColor, Pt, Cm
+from docx.oxml.ns import qn
 
 
 def create_path(path: str) -> None:
@@ -36,65 +39,125 @@ def get_text(xpath_list):
 
 
 def get_p_tag_text(html) -> str:
-    result = html.xpath('./span')
+    result = html.xpath('./span | ./strong')
     if len(result) != 0:
-        return result[0].text
+        return ''.join(result[i].text for i in range(0, len(result)))
     else:
         return '\n'
 
 
-def get_div_tag_img(cookie, html, path) -> None:
-    html = html.xpath('.//img')
-    url = 'https:' + html[0].attrib['src']
-    headers = {
-        'cookie': cookie,
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36'
-    }
-    response = requests.get(url, headers=headers)
-    time.sleep(0.5)
+def get_div_tag_img(cookie, item, path) -> list:
+    html_list = item.xpath('.//img')
 
-    create_path(path)
+    result = list()
+    for html in html_list:
+        if not html.attrib['src']:
+            continue
+        url = 'https:' + html.attrib['src']
+        if "@" in url:
+            url = re.findall(r'(.*)@.*', url)[0]
 
-    suffix = re.findall(r'\.(.*)@', url.split('/')[-1])[0]
+        headers = {
+            'cookie': cookie,
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36'
+        }
+        response = requests.get(url, headers=headers)
+        time.sleep(0.5)
 
-    name = re.findall(rf'(.*)\.{suffix}', url.split('/')[-1])[0]
+        create_path(path)
 
-    with open(f'{path}/{name}.{suffix}', 'wb') as f:
-        f.write(response.content)
+        name = url.split('/')[-1]
 
-    print(f'{path}/{name}.png')
+        with open(f'{path}/{name}', 'wb') as f:
+            f.write(response.content)
+
+        print(f'{path}/{name}')
+        result.append(f'{path}/{name}')
+
+    return result
 
 
-def get_article(cookie: str, article_id: str, storage_location: str = '.') -> None:
+def add_text(doc, text):
+    paragraph = doc.add_paragraph(text)
+
+    for run in paragraph.runs:
+        run.font.color.rgb = RGBColor(47, 50, 56)
+        run.font.size = Pt(17 / 1.5)
+        run.font.name = "黑体"
+        run._element.rPr.rFonts.set(qn('w:eastAsia'), '黑体')
+
+
+def add_image(doc, image_path_list):
+    section = doc.sections[0]
+
+    available_width = (section.page_width - section.left_margin - section.right_margin)
+
+    for image_path in image_path_list:
+        doc.add_picture(image_path, width=available_width)
+
+
+def get_article(cookie: str, article_id: str, doc_storage_location: str = '.', document_name: str = 'Document.doc',img_path: str = 'img') -> None:
     """
     get_article
     :param cookie: website's cookie information.
     :param article_id: article's url such as 1097430290934005800 (https://www.bilibili.com/opus/1097430290934005800?spm_id_from=333.1387.0.0)
-    :param storage_location: local storage in word.
+    :param doc_storage_location: doc local storage in word.
+    :param img_path: img path
     :return: None
     """
     html = get_article_html(cookie, article_id)
     element = etree.HTML(html)
-    title = get_text(element.xpath('//*[@id="app"]/div[4]/div[1]/div[1]/span'))
+    title = get_text(element.xpath('//span[@class="opus-module-title__text"]'))
 
-    author_name = get_text(element.xpath('//*[@id="app"]/div[4]/div[1]/div[2]/div[2]/div[1]'))
+    doc = Document()
 
-    author_time = get_text(element.xpath('//*[@id="app"]/div[4]/div[1]/div[2]/div[2]/div[2]/div'))
+    section = doc.sections[0]
+    section.page_width = Cm(21)  # 宽度 21 厘米
+    section.page_height = Cm(29.7)
+
+    author_name = get_text(element.xpath('//div[@class="opus-module-author__name"]'))
+    author_time = get_text(element.xpath('//div[@class="opus-module-author__pub__text"]'))
 
     if author_time is not None:
-        author_time = datetime.strptime(author_time, '编辑于 %Y年%m月%d日 %H:%M')
+        if '编辑于' in author_time:
+            author_time = datetime.strptime(author_time, '编辑于 %Y年%m月%d日 %H:%M')
+        else:
+            author_time = datetime.strptime(author_time, '%Y年%m月%d日 %H:%M')
 
-    content = element.xpath('//*[@id="app"]/div[4]/div[1]/div[4]/*')
+    if title is not None:
+        doc_title = doc.add_heading(title, level=1)
+        for run in doc_title.runs:
+            run.font.color.rgb = RGBColor(0, 0, 0)
+            run.font.size = Pt(22 / 1.5)
+
+    doc_name = doc.add_paragraph(author_name)
+    doc_time = doc.add_paragraph(author_time.strftime("%Y/%m/%d %H:%M"))
+
+    for run in doc_name.runs:
+        run.font.color.rgb = RGBColor(0, 0, 0)
+        run.font.size = Pt(17 / 1.5)
+        run.font.name = "黑体"
+        run._element.rPr.rFonts.set(qn('w:eastAsia'), '黑体')
+
+    for run in doc_time.runs:
+        run.font.color.rgb = RGBColor(148, 153, 160)
+        run.font.size = Pt(13 / 1.5)
+
+    content = element.xpath('//div[@class="opus-module-content"]/*')
 
     for item in content:
+        print(item.tag)
         if item.tag == 'p':
             print(get_p_tag_text(item))
+            add_text(doc, get_p_tag_text(item))
         elif item.tag == 'div':
-            get_div_tag_img(cookie, item, storage_location)
+            add_image(doc, get_div_tag_img(cookie, item, img_path))
 
     print(content)
 
     print(title, author_name, author_time)
+
+    doc.save(f'{doc_storage_location}/{document_name}')
 
 
 if __name__ == '__main__':
@@ -102,4 +165,4 @@ if __name__ == '__main__':
         cookie = json.load(f)['cookie']
 
     article_id = '1097430290934005800'
-    get_article(cookie, article_id, 'img')
+    get_article(cookie, article_id)
