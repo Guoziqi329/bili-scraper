d196cad7f580ca13690c999d11d4517d723ba3f4
guoziqi329 <guoziqi329@gmail.com>
2025-09-01 12:12:58 +0800

    update get_article.py resolved some bugs.

--- api_reverse/article/get_article.py
+++ api_reverse/article/get_article.py
@@ -10,6 +10,7 @@ from docx.shared import RGBColor, Pt, Cm
 from docx.oxml.ns import qn
 from PIL import Image
 
+session = requests.Session()
 
 def create_path(path: str) -> None:
     if not os.path.exists(path):
@@ -28,15 +29,12 @@ def get_article_html(cookie: str, article_id: str):
         'cookie': cookie,
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36'
     }
-    response = requests.get(url, headers=headers)
+    response = session.get(url, headers=headers)
     return response.text
 
 
-def get_text(xpath_list):
-    if len(xpath_list) == 0:
-        return None
-    else:
-        return xpath_list[0].text
+def get_text(item):
+    pass
 
 
 def get_p_tag_text(html) -> tuple:
@@ -51,36 +49,22 @@ def get_p_tag_text(html) -> tuple:
         return '\n', color, is_strong
 
 
-def get_div_tag_img(cookie, item, path) -> list:
+def get_img(cookie, url_list: list, path) -> list:
     """
     get img.
     :param cookie: website's cookie information
-    :param item: each item under the opus-module-content class
+    :param url_list: each item under the opus-module-content class
     :param path: img path
     :return: image path list
     """
-    html_list = item.xpath('.//img')
-
     result = list()
-    url_list = list()
 
-    for html in html_list:
-        if not html.attrib['src']:
-            continue
-        url = 'https:' + html.attrib['src']
-        if "@" in url:
-            url = re.findall(r'(.*)@.*', url)[0]
-
-        url_list.append(url.split('/')[-1])
-
-    url_list = list(dict.fromkeys(url_list))
-
+    headers = {
+        'cookie': cookie,
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36'
+    }
     for url in url_list:
-        headers = {
-            'cookie': cookie,
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36'
-        }
-        response = requests.get('https://i0.hdslb.com/bfs/new_dyn/' + url, headers=headers)
+        response = session.get(url, headers=headers)
         time.sleep(0.5)
 
         create_path(path)
@@ -91,6 +75,7 @@ def get_div_tag_img(cookie, item, path) -> list:
             f.write(response.content)
 
         print(f'{path}/{name}')
+
         result.append(f'{path}/{name}')
 
     return result
@@ -140,6 +125,21 @@ def add_image(doc, image_path_list) -> None:
         img.close()
         doc.add_picture(image_path, width=available_width)
 
+def add_link(doc, html) -> tuple:
+    """
+    Add link to document.
+    :param doc: docx document
+    :param html: html.
+    :return: link title,
+    """
+
+
+def get_module(INITIAL_STATE: dict, module_name: str):
+    INITIAL_STATE = INITIAL_STATE['detail']['modules']
+    for item in INITIAL_STATE:
+        if module_name in ''.join(list(item.keys())):
+            return item
+    return None
 
 def get_article(cookie: str, article_id: str, doc_storage_location: str = '.', document_name: str = 'Document.doc',
                 img_path: str = 'img') -> None:
@@ -152,54 +152,84 @@ def get_article(cookie: str, article_id: str, doc_storage_location: str = '.', d
     :return: None
     """
     html = get_article_html(cookie, article_id)
+    INITIAL_STATE = re.findall(r'window.__INITIAL_STATE__=(.*);\(function', html)[0]
+    INITIAL_STATE = json.loads(INITIAL_STATE)
+
+    with open('test.json', 'w', encoding='utf-8') as f:
+        json.dump(INITIAL_STATE, f, ensure_ascii=False)
+
     element = etree.HTML(html)
-    title = get_text(element.xpath('//span[@class="opus-module-title__text"]'))
+
+    title = get_module(INITIAL_STATE, 'title')
+    author = get_module(INITIAL_STATE, 'author')
+    module_top = get_module(INITIAL_STATE, 'module_top')
+    module_content = get_module(INITIAL_STATE, 'module_content')
 
     doc = Document()
-
     section = doc.sections[0]
     section.page_width = Cm(21)
     section.page_height = Cm(29.7)
 
-    author_name = get_text(element.xpath('//div[@class="opus-module-author__name"]'))
-    author_time = get_text(element.xpath('//div[@class="opus-module-author__pub__text"]'))
-
-    if author_time is not None:
-        if '编辑于' in author_time:
-            author_time = datetime.strptime(author_time, '编辑于 %Y年%m月%d日 %H:%M')
-        else:
-            author_time = datetime.strptime(author_time, '%Y年%m月%d日 %H:%M')
-
     if title is not None:
+        title = title['module_title']['text']
         doc_title = doc.add_heading(title, level=1)
         for run in doc_title.runs:
             run.font.color.rgb = RGBColor(0, 0, 0)
             run.font.size = Pt(22 / 1.5)
 
-    doc_name = doc.add_paragraph(author_name)
-    doc_time = doc.add_paragraph(author_time.strftime("%Y/%m/%d %H:%M"))
+    if author is not None:
+        author_name = author['module_author']['name']
+        author_time = author['module_author']['pub_time']
 
-    for run in doc_name.runs:
-        run.font.color.rgb = RGBColor(0, 0, 0)
-        run.font.size = Pt(17 / 1.5)
-        run.font.name = "黑体"
-        run._element.rPr.rFonts.set(qn('w:eastAsia'), '黑体')
+        if '编辑于' in author_time:
+            author_time = datetime.strptime(author_time, '编辑于 %Y年%m月%d日 %H:%M')
+        else:
+            author_time = datetime.strptime(author_time, '%Y年%m月%d日 %H:%M')
+    else:
+        author_name = None
+        author_time = None
 
-    for run in doc_time.runs:
-        run.font.color.rgb = RGBColor(148, 153, 160)
-        run.font.size = Pt(13 / 1.5)
+    if author_name is not None and author_time is not None:
+        doc_name = doc.add_paragraph(author_name)
+        doc_time = doc.add_paragraph(author_time.strftime("%Y/%m/%d %H:%M"))
 
-    content = element.xpath('//div[@class="opus-module-content"]/*')
+        for run in doc_name.runs:
+            run.font.color.rgb = RGBColor(0, 0, 0)
+            run.font.size = Pt(17 / 1.5)
+            run.font.name = "黑体"
+            run._element.rPr.rFonts.set(qn('w:eastAsia'), '黑体')
 
-    for item in content:
-        print(item.tag)
-        if item.tag == 'p':
-            print(get_p_tag_text(item))
-            add_text(doc, *get_p_tag_text(item))
-        elif item.tag == 'div':
-            add_image(doc, get_div_tag_img(cookie, item, img_path))
+        for run in doc_time.runs:
+            run.font.color.rgb = RGBColor(148, 153, 160)
+            run.font.size = Pt(13 / 1.5)
 
-    print(content)
+    top_pic_url = list()
+    if module_top is not None:
+        module_top = module_top['module_top']['display']['album']['pics']
+        for pic in module_top:
+            top_pic_url.append(pic['url'])
+
+    add_image(doc, get_img(cookie, top_pic_url, 'img'))
+
+    if module_content is not None:
+        module_content = module_content['module_content']['paragraphs']
+        print(module_content)
+
+    for item in module_content:
+        if item['para_type'] == 1:
+            print('text')
+        elif item['para_type'] == 2:
+            print('image')
+        elif item['para_type'] == 3:
+            print('i don\'t know')
+        elif item['para_type'] == 4:
+            print('code_text')
+        elif item['para_type'] == 5:
+            print('i don\'t know')
+        elif item['para_type'] == 6:
+            print('link_card')
+        print(item)
+        print('-*-'*100)
 
     print(title, author_name, author_time)
 
@@ -210,5 +240,5 @@ if __name__ == '__main__':
     with open('../cookie.json', 'r') as f:
         cookie = json.load(f)['cookie']
 
-    article_id = '1102687328784089096'
+    article_id = '318740526742916934'
     get_article(cookie, article_id,'./', 'document.doc')
